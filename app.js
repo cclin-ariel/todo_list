d063916ed6227b67a4d3d7892e2848556b8c95f3
林雅琦 <wahaha47168@gmail.com>
2021-06-05 11:09:07 +0900

    todo obj, LIST arr edited

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 const clear = document.querySelector(".clear");
 const dateElement = document.getElementById('date');
-const LIST = document.getElementById('list');
+const list = document.getElementById('list');
 const inpute = document.getElementById('inpute');
 
 //classes name
 
 const CHECK = "far fa-check-circle";
-const UNCHECK = "fas fa-sync-alt";
+const UNCHECK = "far fa-circle";
 const LINETHROUGH = "lineThrough";
 
 //show todays date
@@ -17,28 +17,49 @@ const today = new Date();
 const options = { weekday: "long", month: "short", day: "numeric" };
 dateElement.innerHTML = today.toLocaleDateString("en-JP", options);
 
+//set the variable
+
+let LIST = []; 
+var id = 0;
+
+
 //addToDo function
 
-var addToDo = (todo) => {
-    const position = "beforeend";
-    const item = `
+var addToDo = (todo, id, done, trash) => {
+
+    if (trash){ return};
+
+var DONE = done ? CHECK : UNCHECK;
+var LINE = done ? LINETHROUGH : "";
+
+const position = "beforeend";
+const item = `
                 <li class="item">
-                    <i class="far fa-circle" job="complete" id="0"></i>
-                    <p class="text">${todo}</p>
-                    <i class="far fa-trash-alt flex-end" job="delete" id="0"></i>
+                    <i class="${DONE}" job="comlete" id="${id}"></i>
+                    <p class="text ${LINE}">${todo}</p>
+                    <i class="far fa-trash-alt flex-end" job="delete" id="${id}"></i>
                 </li>`;
-    list.insertAdjacentHTML(position, item);
+list.insertAdjacentHTML(position, item);
 };
 
 //add a item to the list by using the enter key
 
-document.addEventListener("keyup", evne =>{
-    if(event.keyCode == 13){
+document.addEventListener("keyup", event => {
+    if (event.keyCode == 13) {
         const todo = input.value;
         //if todo isn't empty
-        if(todo){
-            addToDo(todo);
+        if (todo) {
+            addToDo(todo, id, false, false);
+            LIST.push({
+                name: todo,
+                id: id,
+                done: false,
+                trash: false,
+            });
+            id++;
         };
         input.value = "";
     }
-})
+});
+
+//funcion completeToDo 
